9faead4c07524c8244befa201d33cced35e062c5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-25 01:05:54 +0600

    header logo add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,30 +2,31 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import Image from "react-bootstrap/Image";
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand to="/">
+          <Image
+            className="border rounded"
+            src="https://elearningindustry.com/wp-content/uploads/2015/05/Online-Learning-Teaching-Techniques.jpg"
+            width="30"
+            height="30"
+          />
+        </Navbar.Brand>
+        <Navbar.Brand to="/">BGC Tech</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link to="/home">Home</Nav.Link>
+            <Nav.Link to="#link">Link</Nav.Link>
           </Nav>
+          <div className="d-lg-none">
+            <LeftSideNav></LeftSideNav>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
