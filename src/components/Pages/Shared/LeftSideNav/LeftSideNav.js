041495abe0d8e3126cb8cdb186848716137df277
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-25 11:13:56 +0600

    left Side Categoris add

--- src/components/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/components/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/courses-categories`)
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  // console.log(categories);
   return (
-    <div>
-      <h1>Our Total Courses </h1>
+    <div className="mt-2 nav">
+      <div>
+        {categories?.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
