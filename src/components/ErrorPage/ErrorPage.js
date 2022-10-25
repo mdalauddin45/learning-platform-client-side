0aa893f315c247c7576e006cde7d277d275c1098
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-25 18:04:23 +0600

    error page clr

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>this is error</h1>
+    <div className="text-center mt-5">
+      <h1 className="fs-1">404</h1>
+      <h1>Page not found</h1>
+      <p>
+        The Page you are looking for doesn't exist or an other error eccured.Go{" "}
+        <br />
+        back, or ahead over to example.com to choose a new direction.
+      </p>
+
+      <Link to="/">
+        <Button>Go back to the homepage</Button>
+      </Link>
     </div>
   );
 };
